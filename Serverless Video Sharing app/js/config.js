98667891bb3bf05e84562534235762b8941644fc
afarghaly <33142325+afraghaly10@users.noreply.github.com>
2019-03-18 10:53:14 -0400

    Add files via upload

---
+++ Serverless Video Sharing app/js/config.js
@@ -0,0 +1,7 @@
+var configConstants = {
+    auth0: {
+        domain: 'afarghaly.auth0.com',
+        clientId: 'vMJIm71_jVzMHIsmG6lspTeoKmQCghk9'
+    },
+    apiBaseUrl: 'https://vo10118od3.execute-api.us-east-1.amazonaws.com/dev'
+};
